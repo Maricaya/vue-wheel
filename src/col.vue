ffda719eeb92a59bcd218b8b9563ed6a71d1343b
褚煦露 <chuxulu@lezhixing.com.cn>
2019-08-01 15:14:54 +0800

    row和col的基本模型完成

---
+++ src/col.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <div class="col" :class="[`col-${span}`]"> -->
+  <div class="col" :class="[span ? `col-${span}` : '']">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SweetCol",
+  props: {
+    span: {
+      type: [Number, String]
+    }
+  }
+};
+</script>
+<style scoped lang="scss">
+.col {
+  height: 100px;
+  background-color: grey;
+  width: 100%;
+  border: 1px solid red;
+  // 设置col-1、col-2...一直到col-24，并且给他们设置不同的宽度
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+  // .col.col-1{width:1/24*100%};
+  // .col.col-2{width:2/24*100%}
+  // .....
+  // col.col-24{width:24/24*100%}
+}
+</style>
