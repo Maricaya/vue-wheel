972c88beaa446c17d0b69759c688825f54152c73
褚煦露 <chuxulu@lezhixing.com.cn>
2019-08-06 19:40:41 +0800

    完成header、content、footer

---
+++ src/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SweetFooter"
+};
+</script>
+<style lang='scss' scoped>
+.footer {
+}
+</style>
---
+++ src/sider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sider">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SweetSider'
+};
+</script>
+<style lang='scss' scoped>
+.sider {
+    // position: relative;
+    // width: 100%;
+    // height: 100%;
+}
+</style>
