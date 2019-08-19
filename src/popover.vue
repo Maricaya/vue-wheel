7754a2e03886c0838f1d200f9d577d8c50141be4
褚煦露 <chuxulu@lezhixing.com.cn>
2019-08-19 20:07:50 +0800

    feat: popover 完成top方向

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,12 @@
 <template>
   <!-- @click.stop 事件修饰符 -->
   <div class="popover" ref="popover" @click="onClick">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      v-if="visible"
+      :class="{[`position-${position}`]: true}"
+    >
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
@@ -16,17 +21,38 @@ export default {
   data() {
     return { visible: false };
   },
+  props: {
+    position: {
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "bottom", "left", "right"].indexOf(value) >= 0;
+      }
+    }
+  },
   methods: {
     positionContent() {
-      document.body.appendChild(this.$refs.contentWrapper);
-      let {
-        width,
-        height,
-        top,
-        left
-      } = this.$refs.triggerWrapper.getBoundingClientRect();
-      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
-      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+      const { contentWrapper, triggerWrapper } = this.$refs;
+      document.body.appendChild(contentWrapper);
+      let { width, height, top, left } = triggerWrapper.getBoundingClientRect();
+      if (this.position === "top") {
+        contentWrapper.style.left = left + window.scrollX + "px";
+        contentWrapper.style.top = top + window.scrollY + "px";
+      } 
+      // else if (this.position === "bottom") {
+      //   contentWrapper.style.left = left + window.scrollX + "px";
+      //   contentWrapper.style.top = top + height + window.scrollY + "px";
+      // } else if (this.position === "left") {
+      //   contentWrapper.style.left = left + window.scrollX + "px";
+      //   let { height: height2 } = contentWrapper.getBoundingClientRect();
+      //   contentWrapper.style.top =
+      //     top + window.scrollY + (height - height2) / 2 + "px";
+      // } else if (this.position === "right") {
+      //   contentWrapper.style.left = left + window.scrollX + width + "px";
+      //   let { height: height2 } = contentWrapper.getBoundingClientRect();
+      //   contentWrapper.style.top =
+      //     top + window.scrollY + (height - height2) / 2 + "px";
+      // }
     },
     onClickDocument(e) {
       if (
@@ -36,7 +62,10 @@ export default {
       ) {
         return;
       }
-      if (this.$refs.contentWrapper && (this.$refs.contentWrapper === e.target) || this.$refs.contentWrapper.contains(e.target)) {
+      if (
+        (this.$refs.contentWrapper && this.$refs.contentWrapper === e.target) ||
+        this.$refs.contentWrapper.contains(e.target)
+      ) {
         return;
       }
       this.close();
@@ -53,10 +82,10 @@ export default {
       });
     },
     onClick(event) {
+      console.log(event.target, this.$refs.triggerWrapper)
       if (this.$refs.triggerWrapper.contains(event.target)) {
         if (this.visible === true) {
           this.close();
-          console.log('click close')
         } else {
           this.open();
         }
@@ -81,26 +110,67 @@ $border-radius: 4px;
   filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
   background: white;
   margin-top: -10px;
-  padding: .5em 1em;
+  padding: 0.5em 1em;
   max-width: 20em;
   word-break: break-all;
-  &::before, &::after {
-    content: '';
+  &::before,
+  &::after {
+    content: "";
     display: block;
     border: 10px solid transparent;
     width: 0;
     height: 0;
     position: absolute;
-    left: 10px;
   }
-  &::before {
-    border-top-color: black;
-    top: 100%;
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -10px;
+    &::before,
+    &::after {
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      // 限制高度：容器的高度-1px
+      top: calc(100% - 1px);
+    }
   }
-  &::after {
-    border-top-color: white;
-    // 容器的高度-2px
-    top: calc(100% - 2px);
-  }
+  // &.position-left {
+  //   transform: translateX(-100%);
+  //   margin-left: -10px;
+  //   &::before,
+  //   &::after {
+  //     transform: translateY(-50%);
+  //     top: 50%;
+  //   }
+  //   &::before {
+  //     border-left-color: black;
+  //     left: 100%;
+  //   }
+  //   &::before {
+  //     border-top-color: white;
+  //     left: calc(100% - 1px);
+  //   }
+  // }
+  // &.position-right {
+  //   margin-left: 10px;
+  //   &::before,
+  //   &::after {
+  //     transform: translateY(-50%);
+  //     top: 50%;
+  //   }
+  //   &::before {
+  //     border-right-color: black;
+  //     right: 100%;
+  //   }
+  //   &::after {
+  //     border-right-color: white;
+  //     right: calc(100% - 1px);
+  //   }
+  // }
 }
 </style>
